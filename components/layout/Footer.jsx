5572359ee61f4c0988f04dadc1cceaf10d8546f7
pablomasior <pawel.masior@lotangroup.com>
2024-09-23 20:40:43 +0200

    footer + mobile

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 
-
-const section_footer = "pt-16"
-const section_div = "max-w-5xl px-4 mx-auto wow animate__animated animate__fadeIn"
-const div_content = "flex flex-wrap mb-6 lg:mb-20 -mx-3 text-center lg:text-left grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 "
-const div_block = "w-full lg:w-1/3 px-3 mb-8 lg:mb-0"
-const h2_title = "mb-2 lg:mb-4 lg:text-lg font-bold text-stone-800"
-const a_subtitle = "text-sm lg:text-sm font-medium text-stone-800 hover:text-stone-900 leading-relaxed"
-
 const Footer = () => {
     return (
-        <>
-            <section className={section_footer}>
-                <div className={section_div} data-wow-delay=".3s">
-                    <div className={div_content}>
-                        <a href="/" className="w-full lg:w-2/5 px-3 mb-6 lg:mb-0 wow animate__animated animate__fadeIn animated hover-up-2 duration-500">
-                            <p className="text-center lg:text-left text-stone-900 font-bold text-xl lg:text-2xl ">
-                            🧭 My Holiday Planner
+        <section className="pt-16">
+            <div className="max-w-5xl px-4 mx-auto wow animate__animated animate__fadeIn" data-wow-delay=".3s">
+                <div className="flex flex-wrap mb-6 lg:mb-20 -mx-3 text-center lg:text-left grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4">
+                    <div className="w-full lg:w-2/5 px-3 mb-6 lg:mb-0 wow animate__animated animate__fadeIn animated hover-up-2 duration-500">
+                        <a href="/" className="block">
+                            <p className="text-center lg:text-left text-stone-900 font-bold text-xl lg:text-2xl">
+                                🧭 My Holiday Planner
                             </p>
-                            <p className={"pt-4 lg:pl-10 lg:max-w-full lg:mx-0 lg:text-lg text-stone-900 font-bold"}>
-                            Plan Less, Explore More.
-                            </p>                           
+                            <p className="pt-4 lg:pl-10 lg:max-w-full lg:mx-0 lg:text-lg text-stone-900 font-bold">
+                                Plan Less, Explore More.
+                            </p>
                         </a>
-                        <div className="flex flex-wrap w-full lg:w-3/5">
-                            <div className={div_block}>
-                                <a href="/#" className={h2_title}> o Information</a>
-                            </div>
-                            <div className={div_block}>
-                                <a className={h2_title} href="/contact"> o Contact</a>
-                            </div>
-                            <div className={div_block}>
-                                <a className={h2_title}>Documents</a>
-                                <a href="/document-terms-of-use" className={a_subtitle}><p>Terms of Use</p></a>
-                                <a href="/document-privacy-policy" className={a_subtitle}><p>Privacy Policy</p></a>
-                            </div> 
-                        </div> 
+                    </div>
+                    <div className="flex flex-wrap w-full lg:w-3/5">
+                        <div className="w-full lg:w-1/3 px-3 mb-8 lg:mb-0">
+                            <a href="/#" className="mb-2 lg:mb-4 lg:text-lg font-bold text-stone-800">
+                                Information
+                            </a>
+                        </div>
+                        <div className="w-full lg:w-1/3 px-3 mb-8 lg:mb-0">
+                            <a href="/contact" className="mb-2 lg:mb-4 lg:text-lg font-bold text-stone-800">
+                                Contact
+                            </a>
+                        </div>
+                        <div className="w-full lg:w-1/3 px-3 mb-8 lg:mb-0">
+                            <p className="mb-2 lg:mb-4 lg:text-lg font-bold text-stone-800">Documents</p>
+                            <a href="/document-terms-of-use" className="text-sm lg:text-sm font-medium text-stone-800 hover:text-stone-900 leading-relaxed">
+                                Terms of Use
+                            </a>
+                            <a href="/document-privacy-policy" className="text-sm lg:text-sm font-medium text-stone-800 hover:text-stone-900 leading-relaxed">
+                                Privacy Policy
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </section>
-        </>
+            </div>
+        </section>
     );
 };
 
--- components/layout/MobileMenu.jsx
+++ components/layout/MobileMenu.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import Link from "next/link";
 
-const divHeader = "navbar-backdrop fixed inset-0 bg-stone-800 opacity-25"
-const navHeader = "fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto transition duration-300"
-const ulText = "mobile-menu"
-const ulExpand = "block p-4 text-base text-stone-900 hover:bg-stone-50 hover:text-stone-700 font-bold rounded-xl"
-const sublinkText = "menu-sub-item text-sm text-stone-900 hover:text-stone-800"
-
+const divHeader = "navbar-backdrop fixed inset-0 bg-stone-800 opacity-25";
+const navHeader = "fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto transition duration-300";
+const ulText = "mobile-menu";
+const ulExpand = "block p-4 text-base text-stone-900 hover:bg-stone-50 hover:text-stone-700 font-bold rounded-xl";
+const sublinkText = "menu-sub-item text-sm text-stone-900 hover:text-stone-800";
 
 const MobileMenu = ({ hiddenClass, handleRemove }) => {
     const [isActive, setIsActive] = useState({
@@ -16,65 +15,61 @@ const MobileMenu = ({ hiddenClass, handleRemove }) => {
 
     const handleToggle = (key) => {
         if (isActive.key === key) {
-            setIsActive({
-                status: false,
-            });
+            setIsActive({ status: false });
         } else {
-            setIsActive({
-                status: true,
-                key,
-            });
+            setIsActive({ status: true, key });
         }
     };
+
     return (
-        <>
-            <div className={`${hiddenClass} navbar-menu relative z-50 transition duration-300`}>
-                <div className={divHeader}></div>
-                <nav className={navHeader}>
-                    <Link href="/">
-                        <a className="leading-none wow animate__animated animate__fadeIn animated hover-up-2 duration-500">
-                            <p className="py-4 lg:text-2xl text-lg font-semibold text-stone-900">
+        <div className={`${hiddenClass} navbar-menu relative z-50 transition duration-300`}>
+            <div className={divHeader}></div>
+            <nav className={navHeader}>
+                <button onClick={handleRemove} className="absolute top-4 right-4 text-stone-900">
+                    ✖️
+                </button>
+                <a href="/">
+                    <a className="leading-none wow animate__animated animate__fadeIn animated hover-up-2 duration-500">
+                        <p className="py-4 lg:text-2xl text-lg font-semibold text-stone-900">
                             🧭 My Holiday Planner
-                            </p>
+                        </p>
+                    </a>
+                </a>
+                <div>
+                    <ul className={ulText}>
+                        <li className={`mb-1 menu-item-has-children rounded-xl ${isActive.key === 2 ? "active" : ""}`} onClick={() => handleToggle(2)}>
+                            <span className="menu-expand">+</span>
+                            <Link href="#">
+                                <a className={ulExpand}>🗿 Placeholder_1</a>
+                            </Link>
+                            <ul className={isActive.key === 2 ? "dropdown pl-5" : "hidden"}>
+                                <li><Link href="/contact"><a className={sublinkText}>placeholder_1</a></Link></li>
+                                <li><Link href="/contact"><a className={sublinkText}>placeholder_1</a></Link></li>
+                                <li><Link href="/contact"><a className={sublinkText}>placeholder_1</a></Link></li>
+                            </ul>
+                        </li>
+                        <li className={`mb-1 menu-item-has-children rounded-xl ${isActive.key === 2 ? "active" : ""}`} onClick={() => handleToggle(2)}>
+                            <span className="menu-expand">+</span>
+                            <Link href="#">
+                                <a className={ulExpand}>🗿 Placeholder_2</a>
+                            </Link>
+                            <ul className={isActive.key === 2 ? "dropdown pl-5" : "hidden"}>
+                                <li><Link href="/contact"><a className={sublinkText}>placeholder_2</a></Link></li>
+                                <li><Link href="/contact"><a className={sublinkText}>placeholder_2</a></Link></li>
+                                <li><Link href="/contact"><a className={sublinkText}>placeholder_2</a></Link></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div className="mt-20 space-y-2 pt-6 border-t border-stone-100">
+                        <a href="/contact">
+                            <div className="flex mb-2 p-1 border-stone-600 border-2 rounded-lg w-full justify-center text-base">
+                                Contact us
+                            </div>
                         </a>
-                    </Link>
-                    <div>
-                        <ul className={ulText}>      
-                            <li className={isActive.key == 2 ? "mb-1 menu-item-has-children rounded-xl active" : "mb-1 menu-item-has-children rounded-xl"} onClick={() => handleToggle(2)}>
-                                <span className="menu-expand">+</span>
-                                <Link href="#"><a className={ulExpand}>🗿 Placeholder_1</a></Link>
-                                <ul className={isActive.key == 2 ? "dropdown pl-5" : "hidden"}>
-                                    <li><Link href="/contact"><a className={sublinkText}>placeholder_1</a></Link></li>
-                                    <li><Link href="/contact"><a className={sublinkText}>placeholder_1</a></Link></li>
-                                    <li><Link href="/contact"><a className={sublinkText}>placeholder_1</a></Link></li>
-                                </ul>
-                            </li>      
-                            <li className={isActive.key == 2 ? "mb-1 menu-item-has-children rounded-xl active" : "mb-1 menu-item-has-children rounded-xl"} onClick={() => handleToggle(2)}>
-                                <span className="menu-expand">+</span>
-                                <Link href="#"><a className={ulExpand}>🗿 Placeholder_2</a></Link>
-                                <ul className={isActive.key == 2 ? "dropdown pl-5" : "hidden"}>
-                                    <li><Link href="/contact"><a className={sublinkText}>placeholder_2</a></Link></li>
-                                    <li><Link href="/contact"><a className={sublinkText}>placeholder_2</a></Link></li>
-                                    <li><Link href="/contact"><a className={sublinkText}>placeholder_2</a></Link></li>
-                                </ul>
-                            </li>      
-                        </ul>
-                        <div className="mt-20 space-y-2 pt-6 border-t border-stone-100">
-                            <a href="/contact">
-                                <div className={"flex mb-2 p-1 border-stone-600 border-2 rounded-lg w-full  justify-center text-base"}>
-                                    Contact us
-                                </div>
-                            </a>
-                            {/* <a href="https://profile.lotangroup.com/">
-                            <div className={"flex mb-2 p-1 bg-stone-600 border-2 rounded-lg w-full  justify-center text-base text-white"}>
-                                    Login
-                                </div>
-                            </a> */}
-                        </div>
                     </div>
-                </nav>
-            </div>
-        </>
+                </div>
+            </nav>
+        </div>
     );
 };
 
